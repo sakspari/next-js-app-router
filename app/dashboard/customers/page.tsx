fc4ff22194851e12f4cb429e4c146d1f71058c60
sakspari <gitfreder@gmail.com>
2023-11-30 00:41:09 +0700

    commit for setup databases

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <p>customer page</p>
+  )
+}
+
+export default page
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <p>Dashboard Page</p>
+  )
+}
+
+export default page
