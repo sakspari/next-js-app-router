7dd3be0662ec5832489c952d7b7cdf42d1ef08cf
sakspari <gitfreder@gmail.com>
2023-11-30 01:34:22 +0700

    style(ui): apply loading skeleton

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DashboardSkeleton from '../ui/skeletons'
+
+type Props = {}
+
+const loading = (props: Props) => {
+  return (
+    <DashboardSkeleton/>
+  )
+}
+
+export default loading
