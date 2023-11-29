1d197ab532f4e355e73a70752d6a9c90542a114f
sakspari <gitfreder@gmail.com>
2023-11-30 04:27:20 +0700

    feat: error handling pages
    handle error with error.tsx
    handle error with not-found.tsx
    (optional)  global-error.js -> high level components like layout or template

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
 
 const page = async ({ params }: { params: { id: string } }) => {
   const id = params.id;
@@ -10,6 +11,9 @@ const page = async ({ params }: { params: { id: string } }) => {
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
+  if (!invoice) {
+    notFound();
+  }
   return (
     <main>
       <Breadcrumbs
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -77,6 +77,8 @@ export async function updateInvoice(id: string, formData: FormData) {
 
 // delete invoice
 export async function deleteInvoice(id: string) {
+  throw new Error('Failed to Delete Invoice');
+
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
